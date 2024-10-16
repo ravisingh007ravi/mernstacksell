fbd5f62bf3ce19e18a37ccdcb1f1d9070727e398
ravisingh007ravi <ravisingh007ravi@gmail.com>
2024-10-16 17:16:07 +0530

    .

--- server/src/controller/usercontroller.js
+++ server/src/controller/usercontroller.js
@@ -51,8 +51,7 @@ exports.getApI = async (req, res) => {
 
     catch (e) {
         return res.status(500).send({ status: false, msg: e.message })
-
-    }
+    }
 }
 
 exports.LogInUser = async (req, res) => {
